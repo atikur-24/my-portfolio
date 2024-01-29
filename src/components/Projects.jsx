deb0bf6c982b0f4345cbd8de718ef1eb48cfb4d4
atikur-24 <atikurrahman7223@gmail.com>
2024-01-29 14:09:16 +0600

    added new project name: personal finance tracker

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,22 +5,31 @@ import SectionTitle from "./sheared/SectionTitle";
 
 const Project = () => {
     const [projects, setProjects] = useState(projectData);
+    const [isShowAll, setIsShowAll] = useState(false);
 
     useEffect(() => {
         setProjects(projectData);
     }, []);
 
+    const handleShowAll = () => {
+        setIsShowAll((prevIsShowAll) => !prevIsShowAll);
+    };
+
     return (
         <section className="my-container" id="projects">
             <SectionTitle>Latest Projects</SectionTitle>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
-                {projects?.slice(0, 6).map((project) => (
+                {projects?.slice(0, isShowAll ? projects.length : 6)?.map((project) => (
                     <ProjectCard key={project.id} project={project} />
                 ))}
             </div>
-            <div className="flex justify-center mt-5 lg:mt-10">
-                <button className="my-btn !px-3 !py-2 !text-base !font-medium !rounded">See More</button>
-            </div>
+            {!isShowAll && (
+                <div className="flex justify-center mt-5 lg:mt-10">
+                    <button onClick={handleShowAll} className="my-btn !px-3 !py-2 !text-base !font-medium !rounded">
+                        See More
+                    </button>
+                </div>
+            )}
         </section>
     );
 };
