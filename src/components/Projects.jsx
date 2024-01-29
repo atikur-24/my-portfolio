c667a46be6627afd4790b72fe0a05a4df0002727
atikur-24 <atikurrahman7223@gmail.com>
2024-01-29 13:16:03 +0600

    default show 6 project when click see more button then show all projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,9 +14,12 @@ const Project = () => {
         <section className="my-container" id="projects">
             <SectionTitle>Latest Projects</SectionTitle>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
-                {projects?.map((project) => (
+                {projects?.slice(0, 6).map((project) => (
                     <ProjectCard key={project.id} project={project} />
                 ))}
+            </div>
+            <div className="flex justify-center mt-5 lg:mt-10">
+                <button className="my-btn !px-3 !py-2 !text-base !font-medium !rounded">See More</button>
             </div>
         </section>
     );
